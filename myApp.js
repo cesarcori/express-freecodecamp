1a6822dfe654400dfd0e84ff6282d3fccbb38eeb
jcesar <jcesarcori@gmail.com>
2025-04-30 15:06:01 -0400

    feat: challenge complete express

--- myApp.js
+++ myApp.js
@@ -1,6 +1,69 @@
+require('dotenv').config()
+let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
 
+// app.use((req, res, next) => {console.log(req.method+" "+req.path+" - "+req.iq);next();});
+
+
+app.use((req, res, next) => {
+
+  let string = `${req.method} ${req.path} - ${req.ip}`
+  console.log(string)
+  next();
+
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/now', function (req, res, next) {
+  next();
+}, function (req, res) {
+  req.time = new Date().toString()
+  res.send({ time: req.time });
+});
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Express')
+// })
+
+console.log('dirnmae', __dirname)
+
+const absolutePath = __dirname + '/views/index.html'
+app.get('/', (req, res) => {
+  res.sendFile(absolutePath)
+})
+
+app.get('/json', (req, res) => {
+  console.log('env', process.env.MESSAGE_STYLE)
+  const message = "Hello json"
+  const styleMessage = process.env.MESSAGE_STYLE
+  if (styleMessage === 'uppercase') {
+    res.json({ "message": message.toUpperCase() })
+  } else {
+    res.json({ "message": message })
+  }
+})
+
+// Input parameter
+app.get('/:word/echo', (req, res) => {
+  const { word } = req.params
+  res.json({ echo: word })
+})
+
+// Input query
+app.get('/name', (req, res) => {
+  const { first, last } = req.query
+  res.json({ name: `${first} ${last}` })
+})
+
+app.post('/name', (req, res) => {
+  const { first, last } = req.body
+  res.json({ name: `${first} ${last}` })
+})
+
+app.use('/', express.static(__dirname + '/public'))
 
 
 
@@ -26,14 +89,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
